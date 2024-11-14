0728db0bbebc5e49bc45f6be68a03248beb49f64
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2024-11-13 21:19:37 -0300

    Animação de Ataque com Espada

--- character/character.gd
+++ character/character.gd
@@ -4,6 +4,7 @@ class_name BaseCharacter
 var _has_sword: bool = false
 var _on_floor: bool = true
 var _jump_count: int = 0
+var _attack_index: int = 1
 
 @export_category("Variables")
 @export var _speed: float = 150.0
@@ -11,10 +12,12 @@ var _jump_count: int = 0
 
 @export_category("Objects")
 @export var _character_texture: CharacterTexture
+@export var _attack_combo: Timer
 
 func _physics_process(_delta: float) -> void:
 	_vertical_movement(_delta)
 	_horizontal_movement()
+	_attack_handler()
 
 	move_and_slide()
 	_character_texture.animate(velocity)
@@ -43,6 +46,21 @@ func _horizontal_movement() -> void:
 		return
 	velocity.x = move_toward(velocity.x, 0, _speed)
 
+func _attack_handler() -> void:
+	if not _has_sword:
+		return
+	if Input.is_action_just_pressed("attack"):
+		_character_texture.action_animation("attack_" + str(_attack_index))
+		set_physics_process(false)
+		_attack_combo.start()
+		_attack_index += 1
+		if _attack_index == 4:
+			_attack_index = 1
+
 func update_sword_state(_state: bool) -> void:
 	_has_sword = _state
 	_character_texture.update_suffix(_has_sword)
+
+
+func _on_attack_combo_timeout() -> void:
+	_attack_index = 1
